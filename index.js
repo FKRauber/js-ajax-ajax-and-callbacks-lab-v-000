7a6cc9524c4ae7306d1899c649afbbbe004c4015
Fabienne Rauber <fabienne.rauber@gmail.com>
2019-01-11 05:56:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+var searchRepositories = () => {
+  const searchTerms= $('#searchTerms').val();
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, function(data) {
+    $('#results').html(renderSearchResults(data))
+  }).fail(function(error) {
+    displayError();
+  });
+}
+
+var displayError = () => $('#errors').html("Error.  Please try again.");
+var renderSearchResults = data => data.items.map(item => renderResult(item));
+
+var renderResult = result => {
+  return `
+    <div>
+      <h2><a href="${result.html_url}">${result.name}</a></h2>
+      <p><a href="#" data-repository="${result.name}" data-owner="${result.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
+    </div>
+  `
+}
+
 $(document).ready(function (){
 });
